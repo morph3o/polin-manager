235acfbf8aa14947d6b9f154eb7d8d8fd77bf769
Piero Divasto <pdivasto@gmail.com>
2017-10-17 09:49:03 -0300

    Fix to push to github

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,13 +7,13 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b heroku
-  git add public server.js server.js.map
+  git add . public server.js server.js.map
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin-heroku https://${GH_TOKEN}@github.com/morph3o/polin-manager.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-heroku heroku
+  git push --set-upstream origin-heroku heroku
 }
 
 setup_git
